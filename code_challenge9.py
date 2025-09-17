2028030f30d4dc701f36eb57a168334da97f97ae
Sky Abril <skysanabril@gmail.com>
2025-09-17 10:33:09 +0800

    Create code_challenge9.py
    Done(2) :)

---
+++ code_challenge9.py
@@ -0,0 +1,6 @@
+print("COUNTDOWN TIMER SIMULATION")
+t = eval(input("Enter the starting number for countdown: "))
+print("\nCountdown started: ")
+for x in range(t, 0, -1):
+  print(x)
+print("Liftoff!!")
