1d2dc1ab0ef676659dd081b9bcabdd1182c2b1e0
Sky Abril <skysanabril@gmail.com>
2025-09-22 20:35:28 +0800

    commit code_challenge11.py

---
+++ code_challenge11.py
@@ -0,0 +1,18 @@
+print("\t\t   *")
+for x in range(1,11,1):
+    for y in range(11,x,-1):
+        print(" ", end=' ')
+    for z in range(1,x,1):
+        print("*", end=" ")
+    for t in range(1,x,1):
+        print("*", end=" ")
+    print()
+for x in range(1,11,1):
+    for y in range(1,x,1):
+        print(" ", end=' ')
+    for z in range(11,x,-1):
+        print("*", end=" ")
+    for t in range(11,x,-1):
+        print("*", end=" ")
+    print()
+print("\t\t  " " *")
