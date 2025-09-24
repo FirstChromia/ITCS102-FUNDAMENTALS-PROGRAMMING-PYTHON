9caa8d1b943c943ad8b7afaf918b35c4497b9aab
Sky Abril <skysanabril@gmail.com>
2025-09-24 12:57:05 +0800

    Update code_challenge10.py

--- code_challenge10.py
+++ code_challenge10.py
@@ -1,6 +1,8 @@
-print("\t\t   *")
-for x in range(1,11,1):
-    for y in range(11,x,-1):
+#Triangol
+#5 spaces
+print("\t\t", end="     *")
+for x in range(1,12,1):
+    for y in range(12,x,-1):
         print(" ", end=' ')
     for z in range(1,x,1):
         print("*", end=" ")
